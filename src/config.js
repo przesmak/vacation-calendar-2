7be8384273485731d7ef310f6fdedb4e6b00f0a5
przesmak <przemyslaw.smakulski@gmail.com>
2021-10-09 16:26:32 +0200

    update

---
+++ src/config.js
@@ -0,0 +1,82 @@
+export const calendarConfig = [
+  {
+    id: 0,
+    employee: {
+      name: "jan kowalski"
+    },
+    availability: 100,
+    calendar: [
+      { id: 1, dayState: 1, disabled: false },
+      { id: 2, dayState: 1, disabled: false },
+      { id: 3, dayState: 1, disabled: false },
+      { id: 4, dayState: 1, disabled: false },
+      { id: 5, dayState: 1, disabled: false },
+      { id: 6, dayState: 1, disabled: true },
+      { id: 7, dayState: 1, disabled: true },
+      { id: 8, dayState: 1, disabled: false },
+      { id: 9, dayState: 1, disabled: false },
+      { id: 10, dayState: 1, disabled: false },
+      { id: 11, dayState: 1, disabled: false },
+      { id: 12, dayState: 1, disabled: false },
+      { id: 13, dayState: 1, disabled: true },
+      { id: 14, dayState: 1, disabled: true },
+      { id: 15, dayState: 1, disabled: false },
+      { id: 16, dayState: 1, disabled: false },
+      { id: 17, dayState: 1, disabled: false },
+      { id: 18, dayState: 1, disabled: false },
+      { id: 19, dayState: 1, disabled: false },
+      { id: 20, dayState: 1, disabled: true },
+      { id: 21, dayState: 1, disabled: true },
+      { id: 22, dayState: 1, disabled: false },
+      { id: 23, dayState: 1, disabled: false },
+      { id: 24, dayState: 1, disabled: false },
+      { id: 25, dayState: 1, disabled: false },
+      { id: 26, dayState: 1, disabled: false },
+      { id: 27, dayState: 1, disabled: true },
+      { id: 28, dayState: 1, disabled: true },
+      { id: 29, dayState: 1, disabled: false },
+      { id: 30, dayState: 1, disabled: false },
+      { id: 31, dayState: 1, disabled: false }
+    ]
+  },
+  {
+    id: 1,
+    employee: {
+      name: "Ziomek Niekowalski"
+    },
+    availability: 100,
+    calendar: [
+      { id: 1, dayState: 1, disabled: false },
+      { id: 2, dayState: 1, disabled: false },
+      { id: 3, dayState: 1, disabled: false },
+      { id: 4, dayState: 1, disabled: false },
+      { id: 5, dayState: 1, disabled: false },
+      { id: 6, dayState: 1, disabled: true },
+      { id: 7, dayState: 1, disabled: true },
+      { id: 8, dayState: 1, disabled: false },
+      { id: 9, dayState: 1, disabled: false },
+      { id: 10, dayState: 1, disabled: false },
+      { id: 11, dayState: 1, disabled: false },
+      { id: 12, dayState: 1, disabled: false },
+      { id: 13, dayState: 1, disabled: true },
+      { id: 14, dayState: 1, disabled: true },
+      { id: 15, dayState: 1, disabled: false },
+      { id: 16, dayState: 1, disabled: false },
+      { id: 17, dayState: 1, disabled: false },
+      { id: 18, dayState: 1, disabled: false },
+      { id: 19, dayState: 1, disabled: false },
+      { id: 20, dayState: 1, disabled: true },
+      { id: 21, dayState: 1, disabled: true },
+      { id: 22, dayState: 1, disabled: false },
+      { id: 23, dayState: 1, disabled: false },
+      { id: 24, dayState: 1, disabled: false },
+      { id: 25, dayState: 1, disabled: false },
+      { id: 26, dayState: 1, disabled: false },
+      { id: 27, dayState: 1, disabled: true },
+      { id: 28, dayState: 1, disabled: true },
+      { id: 29, dayState: 1, disabled: false },
+      { id: 30, dayState: 1, disabled: false },
+      { id: 31, dayState: 1, disabled: false }
+    ]
+  }
+];
