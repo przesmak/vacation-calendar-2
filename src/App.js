8ea08d55fbf38e8e9f2755eed59f652eac6a208e
przesmak <przemyslaw.smakulski@gmail.com>
2021-10-09 13:01:18 +0200

    Calendar day change - 3 states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Checkboxes } from "./Checkboxes";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Vacation Calendar</h1>
+      <Checkboxes />
     </div>
   );
 }
---
+++ src/Checkboxes.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const boxes_list = [
+  { id: 0, dayState: 1, disabled: false },
+  { id: 2, dayState: 1, disabled: false },
+  { id: 3, dayState: 1, disabled: false },
+  { id: 4, dayState: 1, disabled: false },
+  { id: 5, dayState: 1, disabled: false },
+  { id: 6, dayState: 1, disabled: true },
+  { id: 7, dayState: 1, disabled: true },
+  { id: 8, dayState: 1, disabled: false },
+  { id: 9, dayState: 1, disabled: false },
+  { id: 10, dayState: 1, disabled: false },
+  { id: 11, dayState: 1, disabled: false },
+  { id: 12, dayState: 1, disabled: false },
+  { id: 13, dayState: 1, disabled: true },
+  { id: 14, dayState: 1, disabled: true },
+  { id: 15, dayState: 1, disabled: false },
+  { id: 16, dayState: 1, disabled: false },
+  { id: 17, dayState: 1, disabled: false },
+  { id: 18, dayState: 1, disabled: false },
+  { id: 19, dayState: 1, disabled: false },
+  { id: 20, dayState: 1, disabled: true },
+  { id: 21, dayState: 1, disabled: true },
+  { id: 22, dayState: 1, disabled: false },
+  { id: 23, dayState: 1, disabled: false },
+  { id: 24, dayState: 1, disabled: false },
+  { id: 25, dayState: 1, disabled: false },
+  { id: 26, dayState: 1, disabled: false },
+  { id: 27, dayState: 1, disabled: true },
+  { id: 28, dayState: 1, disabled: true },
+  { id: 29, dayState: 1, disabled: false },
+  { id: 30, dayState: 1, disabled: false },
+  { id: 31, dayState: 1, disabled: false }
+];
+const Checkbox = (props) => <input type="checkbox" {...props} />;
+
+export function Checkboxes() {
+  const [checkState, setCheckState] = useState(boxes_list);
+
+  const changeCheckState = (id, dayS) => {
+    console.log(id, dayS);
+
+    const oldArray = checkState.filter((v) => v.id !== id);
+    setCheckState([...oldArray, { id: id, dayState: dayS + 1 }]);
+  };
+
+  return (
+    <Wrapper>
+      {checkState
+        .sort((a, b) => a.id - b.id)
+        .map((item) => (
+          <StyledCheckbox
+            onClick={() => changeCheckState(item.id, item.dayState)}
+            {...item}
+          />
+        ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const StyledCheckbox = styled.div`
+  margin-top: 2px;
+  margin-left: 2px;
+  display: flex;
+  width: 36px;
+  height: 36px;
+  background: ${(props) => switchColor(props.dayState)};
+  border-radius: 3px;
+  transition: all 150ms;
+  cursor: pointer;
+  ${(props) =>
+    props.disabled &&
+    `
+      pointer-events: none;
+      opacity: 0.4;
+    `}
+`;
+
+function switchColor(itemValue) {
+  const switcher = itemValue % 3;
+  switch (switcher) {
+    case 0:
+      return "green";
+      break;
+    case 1:
+      return "papayawhip";
+      break;
+    case 2:
+      return "red";
+      break;
+    default:
+      return "black";
+  }
+}
